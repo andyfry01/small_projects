f381705774363bd61516e2d90670cf6c4a321385
Andrew Fry <andyfry01@gmail.com>
2016-06-27 21:52:34 -0400

    got rid of button divs, can grab button classname

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 window.onload = function() {
   console.log("app.js loaded");
   var buttons = document.getElementsByTagName('button')
-  console.log(buttons);
-
 
   for (var i = 0; i < buttons.length; i += 1) {
-    // console.log(buttons[button])
     buttons[i].addEventListener('click', function(e) {
-      console.log(e);
+      console.log(e.target.className);
     })
-
   }
 }
