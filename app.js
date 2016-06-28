c145f9c7481d1cd8f9e70681d8bc01e94f32ccbf
Andrew Fry <andyfry01@gmail.com>
2016-06-27 22:10:36 -0400

    logging time along with button class, although button class isn't always getting logged to the console

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 window.onload = function() {
   console.log("app.js loaded");
+
   var buttons = document.getElementsByTagName('button')
 
+  var timers = {}
+
   for (var i = 0; i < buttons.length; i += 1) {
+
+    timers[i] = buttons[i].className
+
     buttons[i].addEventListener('click', function(e) {
       console.log(e.target.className);
+      console.log(Date.now() / 1000);
     })
+
   }
+  console.log(timers);
 }
