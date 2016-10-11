a6c132f043d55af209f62230f60000e5fd0119e4
Andy Fry <andyfry01@gmail.com>
2016-10-11 16:12:29 -0400

    inputs added for box resizing in HTML, DOM box elements saved as variables in JS

---
+++ public/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+window.onload = function() {
+  console.log('hello from index.js!');
+
+  const FLEXBOX_PARENT = document.getElementById('parent_box_flexbox')
+  const FLEXBOX_CHILD = document.getElementById('child_box_flexbox')
+
+  const MARGIN_PARENT = document.getElementById('parent_box_margin')
+  const MARGIN_CHILD = document.getElementById('child_box_margin')
+
+
+
+
+}
