3bc595bd57be1bd06cb569ff0ee407ff75dbee09
Andy Fry <andyfry01@gmail.com>
2017-06-18 15:04:02 -0400

    more basic tests, test script added to package.json

--- monster-fighter/public/fighterApp.js
+++ monster-fighter/public/fighterApp.js
@@ -1,3 +1,9 @@
 module.exports = {
-  
+  character: function(HP, attackPower){
+    function charObj(HP, attackPower){
+      this.HP = HP
+      this.attackPower = attackPower
+    }
+    return new charObj(HP, attackPower)
+  }
 }
--- monster-fighter/tests/spec.js
+++ monster-fighter/tests/spec.js
@@ -5,14 +5,46 @@ const expect = chai.expect
 
 const fighterApp = require ('../public/fighterApp.js')
 
+function isFunction(functionToCheck) {
+  var getType = {};
+  return functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
+}
+
 describe('fighter app', () => {
   it('should exist', () => {
     let actual = typeof fighterApp
     let expected = 'object'
     assert.equal(actual, expected)
   })
-  describe('', ()=>{
-
+  describe('character creator', () => {
+    it('should exist', () => {
+      let actual = isFunction(fighterApp.character)
+      let expected = true
+      assert.equal(actual, expected)
+    })
+    it('should return an object', () => {
+      let character = fighterApp.character()
+      let actual = typeof character
+      let expected = 'object'
+      assert.equal(actual, expected)
+    })
+    it('should return an object with health stats and attack stats', () => {
+      let character = fighterApp.character()
+      let actual1 = character.hasOwnProperty('HP')
+      let actual2 = character.hasOwnProperty('attackPower')
+      let expected = true
+      assert.equal(actual1, expected)
+      assert.equal(actual2, expected)
+    })
+    it('should take arguments for hp and attack', () => {
+      let character = fighterApp.character(20, 2)
+      let actualHP = character.HP
+      let actualAttack = character.attackPower
+      let expectedHP = 20
+      let expectedAttack = 2
+      assert.equal(actualHP, expectedHP)
+      assert.equal(actualAttack, expectedAttack)
+    })
   })
 })
 
@@ -21,6 +53,12 @@ describe('fighter app', () => {
 
 describe('', ()=>{
 
+})
+
+it('should ', () => {
+  let actual =
+  let expected = true
+  assert.equal(actual, expected)
 })
 
 */
