81e4121579429795f4e9fc17747608ce243fb088
Andy Fry <andyfry01@gmail.com>
2017-07-03 12:50:54 -0400

    set up canvas, ditching p5, testing out canvas settings and test shape

---
+++ frogger/js/Rectangle.js
@@ -0,0 +1,5 @@
+export function Rectangle(w, h) {
+  this.name = "rectangle"
+  this.w = w
+  this.h = h
+}
--- frogger/js/app.js
+++ frogger/js/app.js
@@ -1,3 +1,24 @@
-import test from './test.js'
+import { Rectangle } from './Rectangle'
 
-test.hello()
+window.onload = function(){
+  const canvas = document.getElementsByClassName('canvas')[0]
+  let canvasHeight = window.innerHeight - 16
+  let canvasWidth = window.innerWidth - 16
+  canvas.width = canvasWidth
+  canvas.height = canvasHeight
+  let ctx = canvas.getContext('2d')
+  ctx.fillStyle = "blue"
+  ctx.fillRect(100, 100, 100, 100)
+
+}
+
+/*
+
+  what do we need?
+   a rectangle object
+   a car object
+   a frog object
+   a log object
+   a row object
+
+*/
