19a37ff3b445f961f2b721d80dbb082543d8cd16
Andy Fry <andyfry01@gmail.com>
2017-07-22 08:06:38 -0400

    added collision detection for cars and frog

--- frogger/js/Car.js
+++ frogger/js/Car.js
@@ -1,11 +1,38 @@
 import { Rectangle } from './Rectangle'
 
-export function Car(xpos, ypos, w, h, direction, speed, name) {
+export function Car(xPos, yPos, w, h, direction, speed, name) {
+  // basics
   let car = new Rectangle(w, h)
-  car.xpos = xpos
-  car.ypos = ypos
+  car.xPos = xPos
+  car.yPos = yPos
+  car.name = name
+
+  // used for moving car on update
   car.direction = direction
   car.speed = speed
-  car.name = name
+
+  // used for collision detecting
+  car.topEdge = car.yPos
+  car.rightEdge = car.xPos + car.w
+  car.bottomEdge = car.yPos + car.h
+  car.leftEdge = car.xPos
+
+  // update for each frame
+  car.update = function(){
+    car.xPos += car.speed * car.direction
+    car.rightEdge = car.xPos + car.w
+    car.leftEdge = car.xPos
+  }
+
+  // collision detecting
+  car.detectFrog = function(frog){
+    if (frog.topEdge == car.topEdge) {
+      if (frog.rightEdge > car.leftEdge) {
+        if (frog.leftEdge < car.rightEdge) {
+          console.log(`hi ${car.name}`);
+        }
+      }
+    }
+  }
   return car
 }
--- frogger/js/Frog.js
+++ frogger/js/Frog.js
@@ -1,21 +1,35 @@
 import { Rectangle } from './Rectangle'
 
 export function Frog(xPos, yPos, w, h) {
+  // basics
   let frog = new Rectangle(w, h)
   frog.xPos = xPos
   frog.yPos = yPos
+
+  // used for collision detecting
+  frog.topEdge = frog.yPos
+  frog.rightEdge = frog.xPos + frog.w
+  frog.bottomEdge = frog.yPos + frog.h
+  frog.leftEdge = frog.xPos
+
+  // moves the frog!
   frog.move = function(direction){
     if (direction === 'UP') {
       frog.yPos -= frog.h
+      frog.topEdge = frog.yPos
     }
     if (direction === 'RIGHT') {
       frog.xPos += frog.w
+      frog.rightEdge = frog.xPos + frog.w
     }
     if (direction === 'DOWN') {
       frog.yPos += frog.h
+      frog.topEdge = frog.yPos
+      frog.bottomEdge = frog.yPos + frog.h
     }
     if (direction === 'LEFT') {
       frog.xPos -= frog.w
+      frog.leftEdge = frog.xPos
     }
   }
   return frog
