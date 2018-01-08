c16191daeb36e39d69c344eec566bda5adc34157
Andy Fry <andyfry01@gmail.com>
2018-01-08 17:02:49 -0500

    feat(Utils/ajax.js): added ajax module
    added ajax module for making DB queries

---
+++ jam_time/src/Utils/ajax.js
@@ -0,0 +1,16 @@
+const axios = require('axios')
+module.exports = {
+  getSong(params) {
+    return axios.get( 'http://localhost:3001/songs/fetch', {
+      params: {
+        artist: params.artist,
+        songName: params.songName
+      }
+    })
+  },
+  saveSong(songData) {
+    return axios.post( 'http://localhost:3001/songs/save', {
+      songData
+    })
+  }
+}
