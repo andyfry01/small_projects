6c3d69adb5bec2dcb89c7567b54f3dd91416c116
Andy Fry <andyfry01@gmail.com>
2018-01-09 14:21:17 -0500

    feat: added header and footer stubs
    added stub components for the header and footer

---
+++ jam_time/src/Pages/Home/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../../App.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>This is the app header</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
