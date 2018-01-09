20ae55d608a040c591a1f6b435da0fb1ec07e3db
Andy Fry <andyfry01@gmail.com>
2018-01-09 14:27:04 -0500

    refactor(Home/Index.js): added playlist autoquerying
    added a method to autoquery the two playlists on component load and feed them the data

--- jam_time/src/Pages/Home/Index.js
+++ jam_time/src/Pages/Home/Index.js
@@ -1,20 +1,43 @@
 import React, { Component } from 'react';
 import '../../App.css';
 import SongList from './SongList'
+import Header from './Header'
+import Footer from './Footer'
 import coolCat from '../../Img/Cool_cat.jpg'
-
-const testData = [
-  {name: 'cool cat', img: coolCat, dateAdded: Date.now()},
-  {name: 'cool kat', img: coolCat, dateAdded: Date.now()},
-  {name: 'kool cat', img: coolCat, dateAdded: Date.now()}
-]
+import ajax from '../../Utils/ajax.js'
 
 class Index extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      playlists: {}
+    }
+  }
+  componentDidMount(){
+    Promise.all([
+      ajax.getPlaylist('knownSongs'),
+      ajax.getPlaylist('songsToLearn')
+    ]).then(playlists => {
+      let playListStateUpdate = this.state.playlists
+      playlists.forEach(playlist => {
+        playListStateUpdate[playlist.playlistId] = playlist.songs
+      })
+      this.setState({
+        playlists: playListStateUpdate
+      })
+    })
+  }
   render() {
     return (
       <div>
-        <SongList songListName="test list"
-                  songData={testData} />
+        <Header />
+        <SongList playlistDisplayName="Songs we want to learn"
+                  playlistId="songsToLearn"
+                  songData={this.state.playlists['songsToLearn']} />
+        <SongList playlistDisplayName="Songs we know"
+                  playlistId="knownSongs"
+                  songData={this.state.playlists['songsToLearn']} />
+        <Footer />
       </div>
     );
   }
